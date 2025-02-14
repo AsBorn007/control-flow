64b4eb5cd9b05fae2f6b9b104d89f3c5b0d7c081
AsBorn007 <pk175040@gmail.com>
2025-02-15 03:02:49 +0530

    create truthy.js

---
+++ truthy.js
@@ -0,0 +1,19 @@
+let value = "hello" // this gave us truthy value 
+let falseValue = "" // this gave us flase value 
+let truthyValue = [] // this gave us truthy value 
+
+if(value){
+    console.log("got the value");
+}else{
+    console.log("don't get the value");
+}
+if(falseValue){
+    console.log("got the value");
+}else{
+    console.log("don't get the value");
+}
+if(truthyValue){
+    console.log("got the value");
+}else{
+    console.log("don't get the value");
+}
